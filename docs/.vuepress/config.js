9f088db253d92a75a3982b15b7252842cc144453
Nico <nico_orfi@yahoo.com>
2020-03-28 12:49:43 +0100

    UPD: Docs styling

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,20 +1,57 @@
 module.exports = {
-    title: 'Hello VuePress',
-    description: 'Just playing around',
-    sidebar: {
-        // "/1.0/": require("./1.0"),
-    },
+    title: 'Sigmie docs',
+    description: 'Sigmie library docs',
+    plugins: ['@vuepress/pwa'],
+    head:
+        [
+            [
+                "link",
+                {
+                    href: "https://fonts.googleapis.com/css?family=Montserrat:200,200i,300,300i,400,400i,600,600i,800,800i,900,900i&display=swap",
+                    rel: "stylesheet",
+                    type: "text/css"
+                }
+            ],
+            [
+                "link",
+                {
+                    rel: 'manifest',
+                    href: '/manifest.json'
+                }
+            ],
+            [
+                "link",
+                {
+                    rel: 'icon',
+                    href: '/icon.png'
+                }
+            ]
+        ],
+    nextLinks: true,
+    prevLinks: false,
     themeConfig: {
-        // logo: "/assets/img/logo.svg",
-        displayAllHeaders: true,
-        sidebarDepth: 2,
+        sidebar: [
+            // {
+            //     title: 'Foo',   
+            //     path: '/foo/',      
+            //     collapsable: false,
+            //     sidebarDepth: 1,
+            //     children: [
+            //         '/',
+            //         'foo/one',
+            //         'foo/two',
+            //     ]
+            // },
+            // {
+            //     title: 'Bar',
+            //     children: []
+            // }
+        ],
+        sidebarDepth: 0,
+        logo: "/assets/img/logo.svg",
+        search: false,
         nav: [
-            { text: "Home", link: "https://nova.laravel.com" },
-            {
-                text: "Version",
-                link: "/",
-                items: [{ text: "1.0", link: "/1.0/" }]
-            }
+            { text: "GitHub", link: "https://github.com/sigmie/sigmie" }
         ],
     }
 }
